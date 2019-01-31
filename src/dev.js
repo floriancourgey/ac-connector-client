db0bc4340044cbea431f82d04572d292257be553
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-01-31 17:26:43 -0500

    * refacto

--- src/dev.js
+++ src/dev.js
@@ -1,23 +1,18 @@
-const soap = require('soap');
-var jxon = require('jxon');
-const ACC = require('ac-connector');
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
-
-// const ACCLogManager = ACC.ACCLogManager;
-// const accLogin = ACC.ACCLogManager.getLogin('My Connection Name');
-// if( !accLogin.isLoginRequested() )
-//   accLogin.login();
-//
-// accLogin.getLoginPromise().resolve();
-// console.log('accLogin', accLogin);
+const soap = require('soap');
+var jxon = require('jxon');
+const ACC = require('ac-connector');
 
 const endpoint = process.env.ACC_ENDPOINT
 const wsdl = './wsdl/ac7-8889/xtk_queryDef.wsdl'
 
 soap.createClient(wsdl, {endpoint : endpoint}, function(err, client) {
+  if(err){
+    console.log('soap.createClient error:', err);
+    return;
+  }
   var js = {
     queryDef: {
       '$firstRows':"true",
@@ -28,13 +23,11 @@ soap.createClient(wsdl, {endpoint : endpoint}, function(err, client) {
   }
   var args = {};
   args.sessiontoken = process.env.ACC_SESSIONTOKEN;
-  // args.sessiontoken = accLogin.sessionToken;
   args.entity = { $xml: jxon.jsToString(js)}
   args.duplicate = false
-  // var req = queryDef.SelectAll();
-  client.SelectAll(args, function(err2, result){
-    if(err2){
-      console.log('>>err2', err2, 'err2<<');
+  client.SelectAll(args, function(err, result){
+    if(err){
+      console.log('client.SelectAll error:', err);
       return;
     }
     for(var node of result['entity']['queryDef']['select']['node']){
