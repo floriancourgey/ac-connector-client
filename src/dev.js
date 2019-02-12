6ba66bfbb5ae38ef2164d4215c4261b7681ccafc
Florian Courgey (Fresh) <florian@floriancourgey.com>
2019-02-12 16:47:15 -0500

    + save workflow to *.js

--- src/dev.js
+++ src/dev.js
@@ -5,6 +5,8 @@ const fs = require('fs-extra');
 const soap = require('soap');
 const jxon = require('jxon');
 const xml2js = require('xml2js');
+const xmlParser = new xml2js.Parser({explicitArray: false});
+const xmlBuilder = new xml2js.Builder();
 
 const endpoint = process.env.ACC_ENDPOINT
 const wsdl = './wsdl/ac7-8889/xtk_queryDef.wsdl'
@@ -17,7 +19,7 @@ client.then((client) => {
   var js = {
     queryDef: {
       '$firstRows':"true",
-      '$lineCount':"5",
+      // '$lineCount':"5",
       '$operation':"select",
       '$schema': 'xtk:workflow',
       select: {
@@ -43,31 +45,36 @@ client.then((client) => {
     // console.log('Success! Request:', client.lastRequest);
     // console.log('Success! Response:', client.lastResponse);
     var workflows = [];
-    var _ExecuteQueryResponse = jxon.stringToJs(client.lastResponse)['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ExecuteQueryResponse'];
-    _ExecuteQueryResponse = xml2js.parseString(client.lastResponse, function(err, result){
+    // var _ExecuteQueryResponse = jxon.stringToJs(client.lastResponse)['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ExecuteQueryResponse'];
+
+    var _ExecuteQueryResponse = xmlParser.parseString(client.lastResponse, function(err, result){
       if(err) throw err;
-      console.log(result['SOAP-ENV:Envelope']['SOAP-ENV:Body'][0]['ExecuteQueryResponse'][0]['pdomOutput']);
+      // console.log();
+      for(var workflow of result['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ExecuteQueryResponse']['pdomOutput']['workflow-collection']['workflow']){
+        workflows.push(workflow);
+      }
+      console.log(workflows[0]);
+
+      // write
+      for (workflow of workflows) {
+        var name = workflow['$']['internalName'].replace(/[\:\/\\]/g, '_');
+        var namespace = '_workflow';
+        var path = 'download/'+namespace+'/'+name+'.js';
+        // var content = jxon.jsToString(workflow);
+        var content = xmlBuilder.buildObject(workflow);
+        console.log('   Saving '+namespace+' '+name+' to '+path);
+        // console.log('   with content:', content);
+        fs.outputFileSync(path, content, function (err) {
+          if(err) console.log(err); // => null
+        });
+      }
+      return workflows;
       return;
     });
     return;
+
     // ['SOAP-ENV:Body']['ExecuteQueryResponse'];
 
-    for(var workflow of _ExecuteQueryResponse['pdomOutput']['workflow-collection']['workflow']){
-      workflows.push(workflow);
-    }
-    console.log(workflows[0]);
 
-    // write
-    for (workflow of workflows) {
-      var name = workflow['$internalName'].replace(/[\:\/\\]/g, '_');
-      var namespace = '_workflow';
-      var content = jxon.jsToString(workflow);
-      console.log('   Saving '+namespace+' '+name+' to '+path);
-      console.log('   with content:', content);
-      fs.outputFileSync(path, content, function (err) {
-        if(err) console.log(err); // => null
-      });
-    }
-    return workflows;
   });
 })
